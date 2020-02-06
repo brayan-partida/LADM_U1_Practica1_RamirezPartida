61c728c6d956ed288027e214481f826f32731294
brayan-partida <brenramirezpa@ittepic.edu.mx>
2020-02-06 07:37:44 -0700

    terminado con todos los contenedores

---
+++ app/src/main/java/com/example/ladm_u1_practica1_ramirezpartida/Main4Activity.kt
@@ -0,0 +1,12 @@
+package com.example.ladm_u1_practica1_ramirezpartida
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
